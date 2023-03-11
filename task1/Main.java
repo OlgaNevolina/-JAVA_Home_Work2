303597c20aa2d83764ceb5bbd9fe988eee00d45f
nevol78@mail.ru <nevol78@mail.ru>
2023-03-11 12:56:39 +0300

    task1

--- task1/Main.java
+++ task1/Main.java
@@ -1,74 +1,19 @@
 package task1;
-
+import task1.model.*;;
 public class Main {
 
-    private double x;
-    private double y;
-    private double z;
-    
-    public Main(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    
-    public Main add(Main v) {
-        return new Main(this.x + v.getX(), this.y + v.getY(), this.z + v.getZ());
-    }
-    
-    public Main subtract(Main v) {
-        return new Main(this.x - v.getX(), this.y - v.getY(), this.z - v.getZ());
-    }
-    
-    public double getX() {
-        return x;
-    }
-    
-    public double getY() {
-        return y;
-    }
-    
-    public double getZ() {
-        return z;
-    }
-    
-    public void setX(double x) {
-        this.x = x;
-    }
-    
-    public void setY(double y) {
-        this.y = y;
-    }
-    
-    public void setZ(double z) {
-        this.z = z;
-    }
-    
-    public Main multiply(double scalar) {
-        return new Main(this.x * scalar, this.y * scalar, this.z * scalar);
-    }
-    
-    public double dotProduct(Main v) {
-        return this.x * v.getX() + this.y * v.getY() + this.z * v.getZ();
-    }
-    
-    public Main crossProduct(Main v) {
-        double newX = this.y * v.getZ() - this.z * v.getY();
-        double newY = this.z * v.getX() - this.x * v.getZ();
-        double newZ = this.x * v.getY() - this.y * v.getX();
-        return new Main(newX, newY, newZ);
-    }
-    
-    public double length() {
-        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
-    }
-    
-    @Override
-    public String toString() {
-        return "Vector3D{" +
-                "x=" + x +
-                ", y=" + y +
-                ", z=" + z +
-                '}';
+    public static void main(String[] args) {
+        Vector vector1 = new Vector(1.8,1.9,2.0);
+        Vector vector2 = new Vector(-2.8,2.9,3.0);
+        System.out.println(vector1.toString());
+        System.out.println(vector2.toString());
+        System.out.println("длина: " + vector1.lengthVector());
+        System.out.println("скалярное произведение: " + vector1.scalarMulti(vector2));
+        System.out.println("векторное произведение: " + vector1.vectorMulti(vector2));
+        System.out.println("cos: " + vector1.vectorCos(vector2));
+        System.out.println("сумма: " + vector1.plusVector(vector2));
+        System.out.println("разность: " + vector1.minusVector(vector2));
+        
+
     }
 }
