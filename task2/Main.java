7ed421e7cc3b174c339aa47c0d66cb19ae1da150
nevol78@mail.ru <nevol78@mail.ru>
2023-03-11 12:14:29 +0300

    task1_2

---
+++ task2/Main.java
@@ -0,0 +1,32 @@
+package task2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int primeSum = findPrimeSum(numbers);
+        System.out.println(primeSum);
+    }
+
+    public static int findPrimeSum(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (isPrime(numbers[i])) {
+                sum += numbers[i];
+            }
+        }
+        return sum;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
